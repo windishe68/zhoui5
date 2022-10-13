bde14a719265213048a7243710006788aa904f9c
sapux03 clouddnagmbhgmail.onmicrosoft.com <sapux03@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 07:40:01 +0000

    Initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("at.clouddna.student03.zhoui5.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"atclouddnastudent03/zhoui5/test/unit/controller/Customer.controller"
+], function () {
+	"use strict";
+});
