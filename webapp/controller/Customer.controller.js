986e9321751a0d40be75c5f5d024394e86c1c094
sapux03 clouddnagmbhgmail.onmicrosoft.com <sapux03@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 12:19:16 +0000

    OData Run

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,22 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("at.clouddna.student03.zhoui5.controller.Customer", {
             onInit: function () {
 
             },
-            genderFormatter: function(sKey){
+            onSavePressed: function () {
+                let oView = this.getView();
+                let oModel = oView.getModel();
+                let oData = oModel.getData();
+
+                MessageBox.success(JSON.stringify(oData));
+            },
+
+            genderFormatter: function (sKey) {
                 let oView = this.getView();
                 let oI18nModel = oView.getModel("i18n");
                 let oResourceBundle = oI18nModel.getResourceBundle();
                 let sText = oResourceBundle.getText(sKey);
                 return sText;
-            }           
+            }
         });
     });
---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("at.clouddna.student03.zhoui5.controller.Main", {
+            onInit: function () {
+            }
+        });
+    }
+);
